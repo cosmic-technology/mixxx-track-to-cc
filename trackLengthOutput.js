6f000bbf02bfa547d189e51d74fffa089f3a2556
cosmic-technology <74683601+cosmic-technology@users.noreply.github.com>
2025-06-22 14:43:24 +0100

    Update trackLengthOutput.js

--- trackLengthOutput.js
+++ trackLengthOutput.js
@@ -8,39 +8,63 @@ trackOutput.shutdown = function() {
     print("Shutdown");
 }
 
-function sendTrackLengthAsCC1() {
-    var trackLength = engine.getValue("[Channel1]", "duration");
-    var ccValue = Math.floor((trackLength / 60) * 10);
-    // print("Testing");
-    // print(trackLength);
-    var ccHexValue = ccValue.toString(16);
-    midi.sendShortMsg(0xB0, 0x01, "0x" +ccHexValue);
+function sendTrackTimeRemainingAsCC(deckNum) {
+    var deck = "[Channel" + deckNum + "]";
+    var trackLoaded = engine.getValue(deck, "track_loaded");
+
+    if (trackLoaded === 0) {
+        // No track loaded, send 0 for minutes and seconds
+        midi.sendShortMsg(0xB0, deckNum, 0);  // Send 0 for minutes as CC message
+        midi.sendShortMsg(0xB0, deckNum + 2, 0);  // Send 0 for seconds as CC message
+        return;
+    }
+
+    var trackLength = engine.getValue(deck, "duration");
+    var playPosition = engine.getValue(deck, "playposition");
+    var timeRemaining = trackLength - playPosition;
+
+    var remainingMinutes = Math.floor(timeRemaining / 60);
+    var remainingSeconds = Math.floor(timeRemaining % 60);
+
+    midi.sendShortMsg(0xB0, deckNum, remainingMinutes);  // Send remaining minutes as CC message
+    midi.sendShortMsg(0xB0, deckNum + 2, remainingSeconds);  // Send remaining seconds as CC message
 }
 
-function sendTrackLengthAsCC2() {
-    var trackLength = engine.getValue("[Channel2]", "duration");
-    var ccValue = Math.floor((trackLength / 60) * 10);
-    // print("Testing");
-    // print(trackLength);
-    var ccHexValue = ccValue.toString(16);
-    midi.sendShortMsg(0xB0, 0x02, "0x" +ccHexValue);
-}
-  
-function onTrackLoaded1() {
+function onTrackLoaded(deckNum) {
     var timerId = engine.beginTimer(1000, function() {
-        sendTrackLengthAsCC1();
-        engine.stopTimer(timerId);
+        sendTrackTimeRemainingAsCC(deckNum);
+        if (engine.getValue("[Channel" + deckNum + "]", "play") !== 1) {
+            engine.stopTimer(timerId);
+        }
     });
 }
 
-function onTrackLoaded2() {
-    var timerId = engine.beginTimer(1000, function() {
-        sendTrackLengthAsCC2();
-        engine.stopTimer(timerId);
-    });
-}
-  
 // Add any other necessary functions or event listeners here
 
-engine.connectControl("[Channel1]", "track_loaded", "onTrackLoaded1"); // Call the onTrackLoaded function when a track is loaded in Deck 1
-engine.connectControl("[Channel2]", "track_loaded", "onTrackLoaded2"); // Call the onTrackLoaded function when a track is loaded in Deck 2
+engine.connectControl("[Channel1]", "track_loaded", function(value) {
+    if (value === 1) {
+        onTrackLoaded(1);
+    } else {
+        sendTrackTimeRemainingAsCC(1);  // Track unloaded, send 0 for minutes and seconds
+    }
+});
+
+engine.connectControl("[Channel2]", "track_loaded", function(value) {
+    if (value === 1) {
+        onTrackLoaded(2);
+    } else {
+        sendTrackTimeRemainingAsCC(2);  // Track unloaded, send 0 for minutes and seconds
+    }
+});
+
+engine.connectControl("[Channel1]", "play", function(value) {
+    if (value === 1) {
+        onTrackLoaded(1);
+    }
+});
+
+engine.connectControl("[Channel2]", "play", function(value) {
+    if (value === 1) {
+        onTrackLoaded(2);
+    }
+});
